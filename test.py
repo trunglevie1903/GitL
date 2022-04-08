2a66dae34c7ddb3339f4293c6856a3d482b5baad
trunglevie1903 <trungleve0319@gmail.com>
2022-04-08 10:59:58 +0700

    add test for many players

--- test.py
+++ test.py
@@ -2,11 +2,12 @@ from mylib.player import Player
 import random
 
 def main():
-    p1, p2 = Player('p1'), Player('p2')
-    if random.random() < 0.5:
-        p1.win_against(p2)
-    else:
-        p1.win_against(p2)
+    pL = []
+    for i in range(10):
+        pL.append(Player('p' + str(i)))
+
+    for i in range(len(pL)-1):
+        pL[i].win_against(pL[i+1])
 
 if __name__ == '__main__':
     main()
